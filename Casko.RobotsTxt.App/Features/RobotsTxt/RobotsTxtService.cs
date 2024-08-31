dc7ba1412a731ed1c913450273eee3b6cf49a311
Casper <casper.korsgaard@outlook.com>
2024-08-31 21:31:58 +0200

    Add demo app.

---
+++ Casko.RobotsTxt.App/Features/RobotsTxt/RobotsTxtService.cs
@@ -0,0 +1,25 @@
+﻿using Casko.AspNetCore.RobotsTxt;
+using Casko.AspNetCore.RobotsTxt.Builders;
+using Casko.AspNetCore.RobotsTxt.Validation;
+
+namespace Casko.RobotsTxt.App.Features.RobotsTxt;
+
+public class RobotsTxtService : IRobotsTxt
+{
+    public string GetRobotsTxt(HttpContext httpContext)
+    {
+        var robotsTxt =  new RobotsTxtBuilder(httpContext)
+            .AddSitemap("/sitemap.xml")
+            .AddUserAgent("*")
+            .AddDisallow("/404")
+            .AddDisallow("/private")
+            .AddAllow("/private/login")
+            .Build();
+
+        var robotsTxtValidator = new RobotsTxtValidator(robotsTxt);
+
+        if (!robotsTxtValidator.IsValid) throw new InvalidDataException(nameof(robotsTxt));
+        
+        return robotsTxt;
+    }
+}
